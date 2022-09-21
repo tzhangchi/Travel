2a5b7c8256c365d2e9456985df88fd50d8a1b96c
tzhangchi <terry.zhangchi@outlook.com>
2022-09-21 23:36:00 +0800

    feat: auto commit

--- packages/ui/src/deckjs-canvas.ts
+++ packages/ui/src/deckjs-canvas.ts
@@ -14,7 +14,7 @@ export class DeckjsCanvas extends TwLitElement {
   render(): TemplateResult {
     return html`
       <div
-        class="bg-primary flex-wrap items-center justify-center w-screen h-screen"
+        class="bg-primary flex-wrap items-center justify-center w-screen h-screen overflow-auto"
       >
         <div class="navbar bg-white">
           <div class="navbar-start">
@@ -58,35 +58,74 @@ export class DeckjsCanvas extends TwLitElement {
           </div>
           <div class="basis-4/5">
             <div
-              class="artboard artboard-horizontal bg-white rounded-box w-10/12 h-auto mt-10 m-auto"
+              class="artboard artboard-horizontal bg-white rounded-box w-10/12 h-auto mt-10 m-auto "
             >
-              <div class="hero h-max">
-                <div class="hero-content text-center">
-                  <div class="max-w-md">
-                    <h1 class="text-5xl font-bold">Hello there</h1>
-                    <p class="py-6">
-                      Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                      assumenda excepturi exercitationem quasi. In deleniti
-                      eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button class="btn btn-primary">Get Started</button>
-                  </div>
-                </div>
-              </div>
-              <div class="hero">
-                <div class="hero-content flex-col lg:flex-row">
-                  <img
-                    src="https://placeimg.com/260/400/arch"
-                    class="max-w-sm rounded-lg shadow-2xl"
-                  />
-                  <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">
-                      Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                      assumenda excepturi exercitationem quasi. In deleniti
-                      eaque aut repudiandae et a id nisi.
-                    </p>
-                    <button class="btn btn-primary">Get Started</button>
+              <div class="bg-white py-6 sm:py-8 lg:py-12">
+                <div class="max-w-screen-xl px-4 md:px-8 mx-auto">
+                  <div class="grid md:grid-cols-2 gap-8 lg:gap-12">
+                    <div>
+                      <div
+                        class="h-64 md:h-auto bg-gray-100 overflow-hidden rounded-lg shadow-lg"
+                      >
+                        <img
+                          src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
+                          loading="lazy"
+                          alt="Photo by Martin Sanchez"
+                          class="w-full h-full object-cover object-center"
+                        />
+                      </div>
+                    </div>
+
+                    <div class="md:pt-8">
+                      <p
+                        class="text-indigo-500 font-bold text-center md:text-left"
+                      >
+                        Who we are
+                      </p>
+
+                      <h1
+                        class="text-gray-800 text-2xl sm:text-3xl font-bold text-center md:text-left mb-4 md:mb-6"
+                      >
+                        Our competitive advantage
+                      </h1>
+
+                      <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+                        This is a section of some simple filler text, also known
+                        as placeholder text. It shares some characteristics of a
+                        real written text but is random or otherwise generated.
+                        It may be used to display a sample of fonts or generate
+                        text for testing. Filler text is dummy text which has no
+                        meaning however looks very similar to real text.<br /><br />
+
+                        This is a section of some simple filler text, also known
+                        as placeholder text. It shares some characteristics of a
+                        real written text but is
+                        <a
+                          href="#"
+                          class="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 underline transition duration-100"
+                          >random</a
+                        >
+                        or otherwise generated. It may be used to display a
+                        sample of fonts or generate text for testing. Filler
+                        text is dummy text which has no meaning however looks
+                        very similar to real text.
+                      </p>
+
+                      <h2
+                        class="text-gray-800 text-xl sm:text-2xl font-semibold text-center md:text-left mb-2 md:mb-4"
+                      >
+                        About us
+                      </h2>
+
+                      <p class="text-gray-500 sm:text-lg mb-6 md:mb-8">
+                        This is a section of some simple filler text, also known
+                        as placeholder text. It shares some characteristics of a
+                        real written text but is random or otherwise generated.
+                        It may be used to display a sample of fonts or generate
+                        text for testing. Filler text is dummy text which has no
+                        meaning however looks very similar to real text.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
