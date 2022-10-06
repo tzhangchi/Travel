37b3d47974700d53399e93aed954f986c606740e
tzhangchi <terry.zhangchi@outlook.com>
2022-10-07 00:10:18 +0800

    add stat surface

--- packages/ui/src/deckjs-canvas.ts
+++ packages/ui/src/deckjs-canvas.ts
@@ -7,7 +7,7 @@ import { TwLitElement } from './common/TwLitElement';
 interface Surface {
   title: string;
   id: string;
-  type: string | 'image' | 'article' | 'code';
+  type: string | 'image' | 'article' | 'code' | 'counterdown' | 'stat';
 }
 
 const _uuid = (): string => Math.floor(Math.random() * 1000) + '';
@@ -27,11 +27,14 @@ const _randomTitle = () =>
   ][Math.floor(Math.random() * 10)];
 const _surfaceTitle = (_id?: string) =>
   'S-' + (_id || _uuid()) + ' ' + _randomTitle();
-const _newSurface = function (newType = 'article'): Surface {
+const _newSurface = function (
+  newType = ['image', 'article', 'code', 'counterdown', 'stat'][
+    Math.floor(Math.random() * 5)
+  ]
+): Surface {
   const id = _uuid();
   const title = _surfaceTitle(id);
-  const type =
-    newType || ['image', 'article', 'code'][Math.floor(Math.random() * 3)];
+  const type = newType;
   return {
     id,
     title,
@@ -50,6 +53,65 @@ const _getSurfaceContent = (type: string) => {
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Listen</button>
         </div>
+      </div>
+    </div>`;
+  } else if (type === 'stat') {
+    return html`<div
+      class="stats shadow  flex items-center justify-center gap-2"
+    >
+      <div class="stat">
+        <div class="stat-figure text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-8 h-8 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            ></path>
+          </svg>
+        </div>
+        <div class="stat-title">Total Likes</div>
+        <div class="stat-value text-primary">25.6K</div>
+        <div class="stat-desc">21% more than last month</div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block w-8 h-8 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M13 10V3L4 14h7v7l9-11h-7z"
+            ></path>
+          </svg>
+        </div>
+        <div class="stat-title">Page Views</div>
+        <div class="stat-value text-secondary">2.6M</div>
+        <div class="stat-desc">21% more than last month</div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-figure text-secondary">
+          <div class="avatar online">
+            <div class="w-16 rounded-full">
+              <img src="https://placeimg.com/128/128/people" />
+            </div>
+          </div>
+        </div>
+        <div class="stat-value">86%</div>
+        <div class="stat-title">Tasks done</div>
+        <div class="stat-desc text-secondary">31 tasks remaining</div>
       </div>
     </div>`;
   } else if (type == 'code') {
@@ -79,6 +141,45 @@ const _getSurfaceContent = (type: string) => {
         <a href="#item3" class="btn btn-xs">3</a>
         <a href="#item4" class="btn btn-xs">4</a>
       </div>`;
+  } else if (type == 'counterdown') {
+    return html`<div
+      class="preview border-base-300  rounded-b-box rounded-tr-box flex flex-wrap items-center justify-center gap-2 overflow-x-hidden border bg-cover bg-top p-4 undefined"
+    >
+      <div class="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div
+          class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
+        >
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:15;"></span>
+          </span>
+          days
+        </div>
+        <div
+          class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
+        >
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:10;"></span>
+          </span>
+          hours
+        </div>
+        <div
+          class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
+        >
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:24;"></span>
+          </span>
+          min
+        </div>
+        <div
+          class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content"
+        >
+          <span class="countdown font-mono text-5xl">
+            <span style="--value:56;"></span>
+          </span>
+          sec
+        </div>
+      </div>
+    </div>`;
   }
 };
 @customElement('deckjs-canvas')
@@ -90,7 +191,9 @@ export class DeckjsCanvas extends TwLitElement {
   @property({ type: Array })
   surfaces: Surface[] = [
     _newSurface('article'),
+    _newSurface('stat'),
     _newSurface('image'),
+    _newSurface('counterdown'),
     _newSurface('code'),
   ];
 
