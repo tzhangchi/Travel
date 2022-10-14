27e6c226a8b90305bf8b73cda06afee644ba3a66
tzhangchi <terry.zhangchi@outlook.com>
2022-10-14 22:06:00 +0800

    update btn to icons

--- packages/ui/src/travel-canvas.ts
+++ packages/ui/src/travel-canvas.ts
@@ -284,7 +284,6 @@ export class TravelCanvas extends TwLitElement {
         <div class="navbar bg-white fixed z-50">
           <div class="navbar-start">
             <a class="btn btn-ghost normal-case">Travel</a>
-            <a class="btn  btn-sm" @click=${this._onNewTravel}>New Travel</a>
           </div>
           <div class="navbar-center w-6/12">
             <div class="btn-group">
@@ -299,12 +298,56 @@ export class TravelCanvas extends TwLitElement {
             />
           </div>
           <div class="navbar-end">
-            <a class="btn btn-primary mr-2 btn-sm">Share</a>
+            <a class="mr-4 cursor-pointer" @click=${this._onNewTravel}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="{1.5}"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                />
+              </svg>
+            </a>
+            <a class="mr-4 cursor-pointer"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="{1.5}"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                />
+              </svg>
+            </a>
             <a
-              class="btn btn-secondary btn-sm"
+              class="mr-4 cursor-pointer5"
               @click=${() => (this.isPresentationMode = true)}
-              >Presentation</a
-            >
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="{1.5}"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
+                />
+              </svg>
+            </a>
           </div>
         </div>
         <div class="flex flex-row ">
